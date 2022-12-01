2a3095f9a239f9cbca8cd46c82148bf7ca0df385
KBankou <47104366+BankovK@users.noreply.github.com>
2022-12-01 21:25:24 +0300

    init

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,65 @@
+﻿using AspNetApiApplication.Models;
+using AspNetApiApplication.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetApiApplication.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductController : ControllerBase
+	{
+		public ProductController()
+		{
+		}
+
+		[HttpGet]
+		public ActionResult<List<Product>> GetAll() => ProductService.GetAll();
+
+		[HttpGet("{id}")]
+		public ActionResult<Product> Get(int id)
+		{
+			var product = ProductService.Get(id);
+
+			if (product == null)
+				return NotFound();
+
+			return product;
+		}
+
+		[HttpPost]
+		public IActionResult Create(Product product)
+		{
+			ProductService.Add(product);
+			return CreatedAtAction(nameof(Create), new { id = product.Id }, product);
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult Update(int id, Product product)
+		{
+			if (id != product.Id)
+				return BadRequest();
+
+			var existing = ProductService.Get(id);
+			if (existing == null)
+				return NotFound();
+
+			ProductService.Update(product);
+
+			return NoContent();
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			var existing = ProductService.Get(id);
+
+			if (existing == null)
+				return NotFound();
+
+			ProductService.Delete(id);
+
+			return NoContent();
+		}
+	}
+}
---
+++ Services/ProductService.cs
@@ -0,0 +1,46 @@
+﻿using AspNetApiApplication.Models;
+
+namespace AspNetApiApplication.Services
+{
+	public class ProductService
+	{
+		static List<Product> Products { get; }
+		static int nextId = 3;
+		static ProductService()
+		{
+			Products = new List<Product>
+			{
+				new Product { Id = 1, Name = "Flashlight", Price = 55.55 },
+				new Product { Id = 2, Name = "Sticker", Price = 9.99 }
+			};
+		}
+
+		public static List<Product> GetAll() => Products;
+
+		public static Product? Get(int id) => Products.FirstOrDefault(p => p.Id == id);
+
+		public static void Add(Product product)
+		{
+			product.Id = nextId++;
+			Products.Add(product);
+		}
+
+		public static void Delete(int id)
+		{
+			var product = Get(id);
+			if (product is null)
+				return;
+
+			Products.Remove(product);
+		}
+
+		public static void Update(Product product)
+		{
+			var index = Products.FindIndex(p => p.Id == product.Id);
+			if (index == -1)
+				return;
+
+			Products[index] = product;
+		}
+	}
+}
